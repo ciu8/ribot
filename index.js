b3ab37b3705ffb17b9d2d1d29e6194ed5af2d32c
grassog <gianluca.grasso@ynap.com>
2023-11-09 13:25:10 +0100

    it s just a function

--- index.js
+++ index.js
@@ -17,56 +17,59 @@ const { deletePreference, describeTable } = require("./db_client");
 const { elimina_menu_scene } = require("./scenes/elimina_menu");
 require("dotenv").config();
 
-// Create scene manager
-const stage = new Stage();
-stage.command("cancel", leave());
+function start() {
+  const stage = new Stage();
+  stage.command("cancel", leave());
 
-// Scene registration
-stage.register(
-  salva_menu_scene(),
-  lista_menu_scene(),
-  consulta_menu_scene(),
-  elimina_menu_scene()
-);
-
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
-bot.use(session());
-bot.use(stage.middleware());
-
-/****** START BOT ********/
-
-bot.start((message) => {
-  message.reply(
-    "Ciao, sono RiBot e ti aiuterò a scoprire il menu della mensa scolastica!",
-    Markup.keyboard(MENU_PRINCIPALE).oneTime().resize().extra()
+  // Scene registration
+  stage.register(
+    salva_menu_scene(),
+    lista_menu_scene(),
+    consulta_menu_scene(),
+    elimina_menu_scene()
   );
-});
 
-bot.hears(SALVA_MENU, (ctx) => ctx.scene.enter("salva_menu"));
-bot.hears(LISTA_MENU_SALVATI, (ctx) => ctx.scene.enter("lista_menu"));
-bot.hears(CONSULTA_MENU, (ctx) => ctx.scene.enter("consulta_menu"));
-bot.hears(ELIMINA_MENU, (ctx) => ctx.scene.enter("elimina_menu"));
-bot.hears("test", async (ctx) => {
-  ctx.reply("Do the test");
-});
-bot.on("message", (ctx) =>
-  ctx.reply(
-    "❗ Non ho capito...\nusa /start per iniziare o /cancel per cancellare il comando corrente."
-  )
-);
+  const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+  bot.use(session());
+  bot.use(stage.middleware());
 
-let isLocal = process.env.IS_LOCAL || false;
-if (isLocal) {
-  bot.launch();
-} else {
-  bot.launch({
-    webhook: {
-      domain: process.env.WEBHOOK_DOMAIN,
-      port: process.env.WEBHOOK_PORT,
-    },
+  /****** START BOT ********/
+
+  bot.start((message) => {
+    message.reply(
+      "Ciao, sono RiBot e ti aiuterò a scoprire il menu della mensa scolastica!",
+      Markup.keyboard(MENU_PRINCIPALE).oneTime().resize().extra()
+    );
   });
+
+  bot.hears(SALVA_MENU, (ctx) => ctx.scene.enter("salva_menu"));
+  bot.hears(LISTA_MENU_SALVATI, (ctx) => ctx.scene.enter("lista_menu"));
+  bot.hears(CONSULTA_MENU, (ctx) => ctx.scene.enter("consulta_menu"));
+  bot.hears(ELIMINA_MENU, (ctx) => ctx.scene.enter("elimina_menu"));
+  bot.hears("test", async (ctx) => {
+    ctx.reply("Do the test");
+  });
+  bot.on("message", (ctx) =>
+    ctx.reply(
+      "❗ Non ho capito...\nusa /start per iniziare o /cancel per cancellare il comando corrente."
+    )
+  );
+
+  let isLocal = process.env.IS_LOCAL || false;
+  if (isLocal) {
+    bot.launch();
+  } else {
+    bot.launch({
+      webhook: {
+        domain: process.env.WEBHOOK_DOMAIN,
+        port: process.env.WEBHOOK_PORT,
+      },
+    });
+  }
+
+  // Enable graceful stop
+  process.once("SIGINT", () => bot.stop("SIGINT"));
+  process.once("SIGTERM", () => bot.stop("SIGTERM"));
 }
 
-// Enable graceful stop
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+start();
